1a6da11c731b0551b2683d0542e3ceb80f424ea0
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-04 22:57:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # Your Code Here
+def map(s_array) 
+  new_array = []
+  i = 0 
+ 
+  while i < s_array.length do
+    new_array.push(yield(s_array[i])) 
+    i += 1
+  end
+ 
+  new_array
+end
+
+
+
+def reduce(s_array, s_p=nil)
+  if s_p
+    total = s_p
+    i = 0
+  else 
+    total = s_array[0]
+    i = 1
+  end  
+  
+  while i < s_array.length do
+    total = yield(total, s_array[i])
+    i += 1
+  end  
+  
+  total
+end  
+
