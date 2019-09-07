ea5fb805ff1214366b44e55942061bfe8408ca23
Adam La Rosa <adam.larosa@gmail.com>
2019-09-07 00:18:15 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+
+def map(array)
+  count = 0
+  new = []
+  while count < array.size do
+    new.push(yield(array[count]))
+    count += 1  
+  end
+  return new
+end
+
+def reduce(array, start=nil)
+  if start
+    count = 0
+    total = start
+  else
+    total = array[0]
+    count = 1
+  end
+  
+  while count < array.length
+    total = yield(total, array[count])
+    count += 1
+  end
+  total
+end
