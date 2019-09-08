a4e797e44e6d5bd76e7f1d1eb0bb4a3a3ed4defe
Urfan Guliyev <urfan196@yahoo.com>
2019-09-08 04:06:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+
+def map (array, starting_point)
+  new = []
+ i = 0
+ while i < source_array.length do
+    yield
+   i += 1
+ end
+ return new
+end
+
+map {new.push( source_array[i] * -1 )}
+map {source_array[i]}
+map { source_array[i] * 2  }
+map {source_array[i] ** 2 }
