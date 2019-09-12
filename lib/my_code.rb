415351623d1e841c3c0d8211bc9932c16bcdfdcb
Urfan Guliyev <urfan196@yahoo.com>
2019-09-12 04:18:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,26 @@
 # Your Code Here
 
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield (source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_total (source_array, starting_point = 0)
+total = starting_point
+i = 0
+while i< source_array.length do
+yield 
+  i+=1
+end
+total
+end
+
+
 
 map(array){|n| n * -1})
 map(array){|n| n})
