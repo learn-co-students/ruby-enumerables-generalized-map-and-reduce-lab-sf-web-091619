1815e8058134f0d805e93cc18b34e4b3555efde9
Urfan Guliyev <urfan196@yahoo.com>
2019-09-12 07:03:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def reduce_to_total (source_array, starting_point = 0)
 total = starting_point
 i = 0
 while i< source_array.length do
-yield 
+yield (source_array, starting_point)
   i+=1
 end
 total
